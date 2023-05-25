ab510ed01ff4bd2e704c45a161c5c3bd432fcf20
Ricardo <rs030899@gmail.com>
2023-05-25 12:10:55 -0300

    novos recursos

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,5 @@
+
+// Alterar Tema ->->
 const botaoAlterarTema = document.getElementById("botao-alterar-tema")
 
 const body = document.querySelector("body")
@@ -9,7 +11,7 @@ botaoAlterarTema.addEventListener("click", () => {
     const modoEscuroAtivo = body.classList.contains("modo-escuro")
     body.classList.toggle("modo-escuro")
 
-    
+
     if (modoEscuroAtivo) {
         ImagemBotaoTrocarTema.setAttribute("src", "./src/imagens/sun.png")
     }
@@ -19,3 +21,20 @@ botaoAlterarTema.addEventListener("click", () => {
 
 
 })
+
+// Pesquisa por Nome ->
+
+const inputPesquisa = document.getElementById("pesquisa-nome");
+inputPesquisa.addEventListener("input", () => {
+    const valorPesquisa = inputPesquisa.value.toLowerCase();
+    const cartoesPokemon = document.querySelectorAll(".cartao-pokemon");
+
+    cartoesPokemon.forEach((cartao) => {
+        const nomePokemon = cartao.querySelector(".informacoes span:first-child").textContent.toLowerCase();
+        if (nomePokemon.includes(valorPesquisa)) {
+            cartao.style.display = "flex";
+        } else {
+            cartao.style.display = "none";
+        }
+    });
+});
